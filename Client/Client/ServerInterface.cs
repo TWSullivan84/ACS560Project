d86ad685e5e5e268b03bceb4f240d14f2f9517ff
TWSullivan84 <TWSullivan84@gmail.com>
2016-12-04 20:07:36 -0500

    Server interface initial commit

---
+++ Client/Client/ServerInterface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerSocket
+{
+    public static class ServerInterface
+    {
+        public static void sendToServer(string gameData)
+        {
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry("ec2-35-163-106-205.us-west-2.compute.amazonaws.com");
+                IPAddress ipAddress = ipHostInfo.AddressList[0]; 
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11337);
+
+                Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+                byte[] msg = Encoding.ASCII.GetBytes(gameData);
+                int bytesSent = sender.Send(msg);
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }        
+    }
+}
